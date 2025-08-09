3b78bf340a012b9c8cc4ab1047cbdfb11e789c08
tanabe1105 <asutoronn6521@gmail.com>
2025-08-09 15:21:11 +0900

    ピンごとの定数を設定しました

--- AMP_file/AMP_file.ino
+++ AMP_file/AMP_file.ino
@@ -1,3 +1,15 @@
+const int pwm_a = 2;
+const int derection_a = 30;
+const int pwm_b = 5;
+const int derection_c = 33;
+const int pwm_c = 3;
+const int derection_b = 31;
+const int pwm_d = 6;
+const int derection_d = 34;
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
